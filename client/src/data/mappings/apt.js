fe09f1d3a266204c5e14e75163d0194f0a8afd0c
khoaalski <kbsch28@gmail.com>
2024-11-30 11:16:17 -0500

    worked on the screen for song selection and added mp3 file for apt

---
+++ client/src/data/mappings/apt.js
@@ -0,0 +1,9 @@
+const mapping = [
+    { time: 1.0, action: 'UP' },
+    { time: 2.0, action: 'DOWN' },
+    { time: 3.0, action: 'LEFT' },
+    { time: 4.0, action: 'RIGHT' },
+    // Add more mappings specific to song1
+  ];
+  
+  export default mapping;
--- client/src/data/songs.js
+++ client/src/data/songs.js
@@ -4,11 +4,22 @@ const songs = [
       id: 'cha-cha',
       title: 'Cha Cha Slide',
       file: '/songs/cha-cha.mp3',
+      photo: 'https://cdn-images.dzcdn.net/images/cover/71e421ca919d5936cc5ff9036bfa107d/1900x1900-000000-80-0-0.jpg',
+      difficulty: 'Easy'
     },
     {
         id: 'gangnam-style',
         title: 'Gangnam Style',
         file: '/songs/gangnam-style.mp3',
+        photo: 'https://upload.wikimedia.org/wikipedia/en/a/ad/Gangnam_Style_Official_Cover.png',
+        difficulty: 'Medium'
+    },
+    {
+        id: 'apt',
+        title: 'APT',
+        file: '/songs/apt.mp3',
+        photo: 'https://i.scdn.co/image/ab67616d0000b273ba60245b7725fdc3719027c0',
+        difficulty: 'Hard'
     }
   ];
   
